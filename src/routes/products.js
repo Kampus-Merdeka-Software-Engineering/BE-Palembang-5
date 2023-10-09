fc4df144de650708d668255bf5d14a6ca0a1b66b
Luthfi Taufiqulhakim <132580419+LuthfiTaufiqulhakim@users.noreply.github.com>
2023-10-09 19:38:31 +0700

    Delete src directory

--- src/routes/products.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getAll, getById } from "../controllers/products.js";
-
-const productsRouter = Router();
-
-productsRouter.get("/", getAll);
-productsRouter.get("/:id", getById);
-
-export default productsRouter;
